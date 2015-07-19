dcccf917120b754ab0b779372dec682c9e670f15
pekopeko1 <t.moi.larsson+pekoepko1@gmail.com>
2015-07-20 03:09:28 +0900

    worked

--- golang/ntp_base.go
+++ golang/ntp_base.go
@@ -24,6 +24,7 @@ func main() {
 func handleClient(conn *net.UDPConn, sb []byte) {
 	var buf [256]byte
 	size, addr, err := conn.ReadFromUDP(buf[0:])
+	//println(size, addr)
 	if err != nil {
 		return
 	}
@@ -31,37 +32,44 @@ func handleClient(conn *net.UDPConn, sb []byte) {
 		return
 	}
 	sec, microsec := gettimeofday()
-	vn := byteArrayToUint32(buf[0:][0:3]) & 0x38000000
-	copy(sb[0:3], uint32ToByteArray(0x040106F0|vn)) // flag
+	vn := byteArrayToUint64(buf[0:][0:3]) & 0x38000000
+	copy(sb[0:3], uint64ToByteArray(0x040106F0|vn)) // flag
 	sb[4] = 0                                       // delay
-	copy(sb[8:11], uint32ToByteArray(0x00000010))   // dispersion
+	copy(sb[8:11], uint64ToByteArray(0x00000010))   // dispersion
 	copy(sb[12:15], []byte("LOCL"))                 // Ref ID
-	copy(sb[16:19], uint32ToByteArray(sec+0x83AA7E80))
+	copy(sb[16:19], uint64ToByteArray(sec+0x83AA7E80))
 	copy(sb[20:23], []byte{0, 0, 0, 0})
 	copy(sb[24:31], buf[0:][40:47])
-	copy(sb[32:35], uint32ToByteArray(sec+0x83AA7E80))
-	copy(sb[36:39], uint32ToByteArray((microsec/500000)*0x80000000))
+	copy(sb[32:35], uint64ToByteArray(sec+0x83AA7E80))
+	copy(sb[36:39], uint64ToByteArray((microsec/500000)*0x80000000))
 	sec, microsec = gettimeofday()
-	copy(sb[40:43], uint32ToByteArray(sec+0x83AA7E80))
-	copy(sb[44:47], uint32ToByteArray((microsec/500000)*0x80000000))
+	copy(sb[40:43], uint64ToByteArray(sec+0x83AA7E80))
+	copy(sb[44:47], uint64ToByteArray((microsec/500000)*0x80000000))
 
 	conn.WriteToUDP([]byte(sb), addr)
 }
 
-func uint32ToByteArray(num uint32) (ret []byte) {
-	ret[0] = byte((0xff000000 | num) >> 24)
-	ret[1] = byte((0x00ff0000 | num) >> 16)
-	ret[2] = byte((0x0000ff00 | num) >> 8)
-	ret[3] = byte(0x000000ff | num)
+func uint64ToByteArray(num uint64) (ret []byte) {
+	ret = make([]byte, 4)
+	ret[0] = byte(0xFF000000 & num >> 24)
+	ret[1] = byte(0x00FF0000 & num >> 16)
+	ret[2] = byte(0x0000FF00 & num >> 8)
+	ret[3] = byte(0x000000FF & num)
+	//println(ret[0], ret[1], ret[2], ret[3])
 	return ret
 }
 
-func byteArrayToUint32(ba []byte) (ret uint32) {
-	if len(ba) != 4 {
-		panic(0)
+func byteArrayToUint64(ba []byte) (ret uint64) {
+	switch len(ba) {
+	case 4:
+		//right way
+	case 3:
+		ba = append(ba, 0)
+	default:
+		panic(len(ba))
 	}
 	for i := 0; i < 4; i++ {
-		ret += uint32(ba[i]) << uint32(24-(8*i))
+		ret += uint64(ba[i]) << uint64(24-(8*i))
 	}
 	return ret
 }
@@ -73,9 +81,9 @@ func checkError(err error) {
 	}
 }
 
-func gettimeofday() (sec uint32, microsec uint32) {
+func gettimeofday() (sec uint64, microsec uint64) {
 	now := time.Now().UnixNano()
-	sec = uint32(now / 1e9)
-	microsec = uint32(now / 1e6)
+	sec = uint64(now / 1e9)
+	microsec = uint64(now / 1e6)
 	return sec, microsec
 }
